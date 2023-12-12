5e33c62565bcaa964e2f2a7f19666264aa39cd6d
David Rey <neisserrey@upeu.edu.pe>
2023-12-12 11:14:26 -0500

    1ER INTENTO DEPLOY A RAILWAY :)

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ogess_frontend/',
+  base: '/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
